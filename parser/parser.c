626a0de828b320150a8fe3adff3f24f2015e42ac
Minseo Park <minsepar@c4r3s5.42seoul.kr>
2024-02-05 17:07:54 +0900

    added parser directory, parser.c, parser.h, defined linked list struct

--- parser/parser.c
+++ parser/parser.c
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/05 16:22:19 by minsepar          #+#    #+#             */
-/*   Updated: 2024/02/05 17:07:17 by minsepar         ###   ########.fr       */
+/*   Created: 2024/02/05 16:52:54 by minsepar          #+#    #+#             */
+/*   Updated: 2024/02/05 16:52:55 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAIN_H
-# define MAIN_H
-
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <unistd.h>
-# include <unistd.h>
-
-typedef struct s_minishell
-{
-    char    *cwd;
-    char    *input_str;
-    
-}   t_minishell;
-
-#endif
--- parser/parser.h
+++ parser/parser.h
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/05 16:22:19 by minsepar          #+#    #+#             */
-/*   Updated: 2024/02/05 17:07:17 by minsepar         ###   ########.fr       */
+/*   Created: 2024/02/05 16:52:51 by minsepar          #+#    #+#             */
+/*   Updated: 2024/02/05 17:05:22 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAIN_H
-# define MAIN_H
+#ifndef PARSER_H
+# define PARSER_H
 
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <unistd.h>
-# include <unistd.h>
-
-typedef struct s_minishell
+typedef struct s_list_node
 {
-    char    *cwd;
-    char    *input_str;
-    
-}   t_minishell;
+	struct s_list_node	*next;
+	char				*str;
+}	t_list_node;
+
+typedef struct t_linked_list
+{
+	size_t		size;
+	t_list_node *front;
+	t_list_node	*back;
+}	t_linked_list;
 
 #endif
