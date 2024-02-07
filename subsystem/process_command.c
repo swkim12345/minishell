f17a8a8acdfd4903aa6e0320f404cdc4f8ca0301
Minseo Park <minsepar@c5r5s3.42seoul.kr>
2024-02-07 13:38:00 +0900

    save for mege

--- subsystem/process_command.c
+++ subsystem/process_command.c
@@ -6,15 +6,16 @@
 /*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 17:20:26 by minsepar          #+#    #+#             */
-/*   Updated: 2024/02/07 13:31:55 by minsepar         ###   ########.fr       */
+/*   Updated: 2024/02/07 13:37:01 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parser/parser.h"
+
 /*
 	cmd struct list
 */
-int	process_command()
+int	process_command(t_cmd_node *cmd_node)
 {
 	
 }
