1a845c721002d2965c932f575a2c4ea34a82f109
Minseo Park <minsepar@c5r5s3.42seoul.kr>
2024-02-07 13:53:46 +0900

    feat add process_command()

--- subsystem/process_command.c
+++ subsystem/process_command.c
@@ -6,16 +6,27 @@
 /*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 17:20:26 by minsepar          #+#    #+#             */
-/*   Updated: 2024/02/07 13:37:01 by minsepar         ###   ########.fr       */
+/*   Updated: 2024/02/07 13:52:53 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../parser/parser.h"
+#include "../main.h"
 
 /*
 	cmd struct list
 */
+
+int	is_builtin_fn(t_cmd_node *cmd_node)
+{
+	char	*str;
+
+	str = cmd_node->cmd_name;
+	if (str_equal)
+}
+
 int	process_command(t_cmd_node *cmd_node)
 {
-	
+	cmd_node->cmd_name = cmd_node->str[0];
+	if (is_builtin_fn(cmd_node))
+		process_builtin(cmd_node);
 }
