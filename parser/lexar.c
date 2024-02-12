30ff0332996554bee9de2c80901ec73c2162b7af
Sunghwan Kim <sunghwki@c6r7s8.42seoul.kr>
2024-02-12 20:47:03 +0900

    Revert "Revert "Revert "Revert "normalizing finder, add redirection""""
    This reverts commit 14a03b81ae700f3a2e47a2f6ec1d0618460fa303.

--- parser/lexar.c
+++ parser/lexar.c
@@ -6,7 +6,7 @@
 /*   By: sunghwki <sunghwki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 13:28:22 by sunghwki          #+#    #+#             */
-/*   Updated: 2024/02/07 21:12:39 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/02/12 20:08:21 by sunghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,16 +52,16 @@ t_ast_node	*init_ast_node(int child_node)
 	
 	ret = (t_ast_node *)malloc(sizeof(t_ast_node));
 	ft_memset((void *)ret, 0, sizeof(ret));
-	if (CMD_NODE & child_node)
+	if (CMDNODE & child_node)
 	{
 		node = (t_cmd_node *)malloc(sizeof(t_cmd_node));
 		ft_memset((void *)node, 0, sizeof(node));
 		ret->cmd_node = node;
 	}
-	if (LEFT_NODE & child_node)
-		ret->left_node = init_ast_node(CMD_NODE);
-	if (RIGHT_NODE & child_node)
-		ret->right_node = init_ast_node(CMD_NODE);
+	if (LEFTNODE & child_node)
+		ret->left_node = init_ast_node(CMDNODE);
+	if (RIGHTNODE & child_node)
+		ret->right_node = init_ast_node(CMDNODE);
 	return (ret);
 }
 
@@ -79,7 +79,7 @@ static	t_ast_node	*pipe_lexar(t_ast_node *head)
 	if (ptr[index])
 	{
 		//split ast tree
-		ret = init_ast_node(CMD_NODE);
+		ret = init_ast_node(CMDNODE);
 		ret->next_ast_node = head;
 		ptr[index] = '\0';
 		str = ft_strdup(ptr);
@@ -93,35 +93,35 @@ static	t_ast_node	*pipe_lexar(t_ast_node *head)
 	return (head);
 }
 
-static	long	bracket_lexar(t_ast_node *head)
-{
-	long		index;
-	char		*ptr;
-	long		bracket_count;
-	long		tmp;
+// static	long	bracket_lexar(t_ast_node *head)
+// {
+// 	long		index;
+// 	char		*ptr;
+// 	long		bracket_count;
+// 	long		tmp;
 
-	ptr = head->cmd_node->str[0];
-	index = find_or_and_bracket(ptr);
-	if (!ft_strncmp(ptr, &BRACKET[0], 1))
-	{
-		bracket_count = 1;
-		tmp = index;
-		ft_strlcpy(&ptr[index], &ptr[index + 1], ft_strlen(&ptr[index + 1]));
-		while (TRUE)
-		{
-			tmp = find_bracket(&ptr[index]);
-			if (ptr[tmp] == BRACKET[0])
-				bracket_count++;
-			else
-				bracket_count--;
-			if (!bracket_count || !ptr[tmp])
-				break;
-			tmp++;
-		}
-		ft_strlcpy(&ptr[tmp], &ptr[tmp + 1], ft_strlen(&ptr[tmp + 1]));
-	}
-	return (index);
-}
+// 	ptr = head->cmd_node->str[0];
+// 	index = find_or_and_bracket(ptr);
+// 	if (!ft_strncmp(ptr, &BRACKET[0], 1))
+// 	{
+// 		bracket_count = 1;
+// 		tmp = index;
+// 		ft_strlcpy(&ptr[index], &ptr[index + 1], ft_strlen(&ptr[index + 1]));
+// 		while (TRUE)
+// 		{
+// 			tmp = find_bracket(&ptr[index]);
+// 			if (ptr[tmp] == BRACKET[0])
+// 				bracket_count++;
+// 			else
+// 				bracket_count--;
+// 			if (!bracket_count || !ptr[tmp])
+// 				break;
+// 			tmp++;
+// 		}
+// 		ft_strlcpy(&ptr[tmp], &ptr[tmp + 1], ft_strlen(&ptr[tmp + 1]));
+// 	}
+// 	return (index);
+// }
 
 static	t_ast_node	*or_and_lexar(t_ast_node *head)
 {
@@ -136,7 +136,7 @@ static	t_ast_node	*or_and_lexar(t_ast_node *head)
 	index = find_or_and_bracket(ptr);
 	if (!ft_strncmp(&ptr[index], OR, 2) || !ft_strncmp(&ptr[index], AND, 2))
 	{
-		ret = init_ast_node(RIGHT_NODE);
+		ret = init_ast_node(RIGHTNODE);
 		ret->next_ast_node = head->next_ast_node;
 		if (!ft_strncmp(&ptr[index], OR, 2))
 			str = ft_strdup(OR);
@@ -170,7 +170,7 @@ t_ast_node	*recur_lexar(t_ast_node *head)
 	index = find_or_and_bracket(ptr);
 	if (!ptr[index])
 		return (head);
-	bracket_lexar(head);
+	// bracket_lexar(head);
 	ret = or_and_lexar(head);
 	if (ret != head)
 		return (ret);
@@ -182,7 +182,7 @@ t_ast_node	*lexar(char *input)
 	t_ast_node	*head;
 	char		*i;
 
-	head = init_ast_node(CMD_NODE);
+	head = init_ast_node(CMDNODE);
 	i = ft_strdup(input);
 	head->cmd_node->str = init_doub_char(&i, 1);
 	head = recur_lexar(head);	
--- parser/parser.c
+++ parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: sunghwki <sunghwki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 16:52:54 by minsepar          #+#    #+#             */
-/*   Updated: 2024/02/07 20:28:51 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/02/12 20:08:07 by sunghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,20 @@
 ||, && 처리는 lexar에서 처리가 완료된 것으로 가정함.
 */
 
-// t_ast_node	*parser(char *input)
-// {
-// 	t_ast_node	*head;
+char		**parse_str(char *input)
+{
+	/*
+	space, tab, newline
+	redirection(>, >>, <, <<)
+	$ (dollar sign) - 이거 구현해야 함?
+	*/
 	
-// 	head = lexar(input);
-// 	//split with space
+}
+
+t_ast_node	*parser(char *input)
+{
+	t_ast_node	*head;
 	
-// 	//splitting
-// 	//merge
-// }
+	head = init_ast_node(CMDNODE);
+	
+}
