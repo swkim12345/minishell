677f9f0ce3a6a9e33fb82df2fec6e98f689e9b53
minsepar <parkj12b@github.com>
2024-02-21 19:30:41 +0900

    feat add error()

--- subsystem/subsystem.h
+++ subsystem/subsystem.h
@@ -6,7 +6,7 @@
 /*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 13:39:54 by minsepar          #+#    #+#             */
-/*   Updated: 2024/02/21 14:27:04 by minsepar         ###   ########.fr       */
+/*   Updated: 2024/02/21 15:17:10 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@
 typedef struct s_cmd_node	t_cmd_node;
 /* process_command.c*/
 
-int	process_command(t_cmd_node *cmd_node);
+int	process_command(t_cmd_node *cmd_node, t_minishell *minishell);
 
 #endif
