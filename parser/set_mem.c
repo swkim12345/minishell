0845803b3c651fe9482183fea8dc8d10a840430d
sunghwki <swkim12345@naver.com>
2024-02-25 16:29:19 +0900

    add set_mem

---
+++ parser/set_mem.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_mem.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunghwki <sunghwki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 15:36:45 by sunghwki          #+#    #+#             */
+/*   Updated: 2024/02/25 15:48:45 by sunghwki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+char	**init_doub_char(char **input, int size)
+{
+	char	**ret;
+	int		i;
+
+	i = -1;
+	ret = (char **)malloc(sizeof(char *) * (size + 1));
+	while (++i < size && input[i])
+		ret[i] = ft_strdup(input[i]);
+	ret[i] = NULL;
+	return (ret);
+}
+
+char	*dup_str(char *str, int start, int end)
+{
+	char	*ret;
+
+	ret = (char *)malloc(sizeof(char) * (end - start + 1));
+	ret[end - start] = '\0';
+	ft_memmove((void *)ret, (void *)&str[start], end - start);
+	return (ret);
+}
+
+void	free_redirection(t_redirection *red)
+{
+	t_redirection	*tmp;
+
+	while (red)
+	{
+		tmp = red->next;
+		free_doub_char(&red->str);
+		free(red);
+		red = tmp;
+	}
+}
+
+void	free_doub_char(char **input)
+{
+	int	i;
+
+	i = -1;
+	while (input[++i])
+		free(input[i]);
+	free(input[i]);
+	free(input);
+}
+
+void	free_cmd_node(t_cmd_node *node)
+{
+	int			i;
+
+	i = -1;
+	if (node == NULL)
+		return ;
+	if (node->cmd_name)
+		free(node->cmd_name);
+	if (node->str)
+		free_doub_char(node->str);
+	free(node);
+}
+
+void	free_ast_tree(t_ast_node *node)
+{
+	if (!node)
+		return ;
+	if (node->next_ast_node)
+		free_ast_tree(node->next_ast_node);
+	if (node->left_node)
+		free_ast_tree(node->left_node);
+	if (node->right_node)
+		free_ast_tree(node->right_node);
+	if (node->cmd_node)
+		free_cmd_node(node->cmd_node);
+	if (node->str)
+		free(node->str);
+	free(node);
+	return ;
+}
