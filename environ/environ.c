02b061e109e852f019cb73b3427112818b966bd4
sunghwki <swkim12345@naver.com>
2024-02-26 17:02:13 +0900

    adding lexar

--- environ/environ.c
+++ environ/environ.c
@@ -6,7 +6,7 @@
 /*   By: sunghwki <sunghwki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 12:03:39 by sunghwki          #+#    #+#             */
-/*   Updated: 2024/02/26 14:59:06 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/02/26 15:05:16 by sunghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,6 @@ int	ft_unsetenv(t_tree_head *head, char *key)
 t_tree_head	*ft_initenv(char **envp)
 {
 	t_tree_head	*head;
-	t_tree_node	*tmp;
 	int			index;
 
 	head = (t_tree_head *)malloc(sizeof(t_tree_head));
