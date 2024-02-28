0ab302bdac5ed5b2d2843adf2576c854a2f7fa30
minsepar <parkj12b@github.com>
2024-02-28 12:52:40 +0900

    .

--- builtin/ft_unset.c
+++ builtin/ft_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sunghwki <sunghwki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:38:13 by sunghwki          #+#    #+#             */
-/*   Updated: 2024/02/26 20:11:53 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/02/28 12:52:33 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_unset(t_cmd_node *cmd_node, t_minishell *minishell)
 				cmd_node->str[index], "not a valid identifier");
 		if (!ft_isalpha(cmd_node->str[index][0]))
 			print_error_msg(minishell->error, 1, TRUE);
-		free_error(minishell->error);
+		// free_error(minishell->error);
 		if (ft_unsetenv(minishell->export, cmd_node->str[index]) == FUNC_FAIL)
 			return (FUNC_FAIL);
 		if (ft_unsetenv(minishell->env, cmd_node->str[index]) == FUNC_FAIL)
