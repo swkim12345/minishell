811ebe4a400c6e955018dd9fd96e291bcdff0b71
minsepar <parkj12b@github.com>
2024-03-09 13:10:39 +0900

    delete debug msg

--- builtin/ft_env.c
+++ builtin/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:18:17 by sunghwki          #+#    #+#             */
-/*   Updated: 2024/03/08 16:25:16 by minsepar         ###   ########.fr       */
+/*   Updated: 2024/03/09 13:10:19 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ int	ft_env(t_minishell *minishell)
 			if (ret[index][i] != '=')
 				continue ;
 		}
-		ft_printf("%s\n", ret[index]);
 		free(ret[index]);
 	}
 	free(ret);
