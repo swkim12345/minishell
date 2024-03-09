837ed30133b7e4a0895a39db9b076d7539db1a2a
sunghwki <swkim12345@naver.com>
2024-03-09 12:48:38 +0900

    test

--- builtin/ft_unset.c
+++ builtin/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: sunghwki <sunghwki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:38:13 by sunghwki          #+#    #+#             */
-/*   Updated: 2024/02/29 13:43:19 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/03/09 12:48:14 by sunghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,16 @@ static int	ft_not_unset(t_cmd_node *cmd_node, t_minishell *minishell,
 int	ft_unset(t_cmd_node *cmd_node, t_minishell *minishell)
 {
 	int		index;
+	int		tmp;
 
 	index = 0;
+	tmp = FUNC_SUC;
 	while (cmd_node->str[++index])
 	{
 		if (ft_not_unset(cmd_node, minishell, index) == FUNC_FAIL)
-			continue ;
+			tmp = FUNC_FAIL;
 		ft_unsetenv(minishell->export, cmd_node->str[index]);
 		ft_unsetenv(minishell->env, cmd_node->str[index]);
 	}
-	return (FUNC_SUC);
+	return (tmp);
 }
