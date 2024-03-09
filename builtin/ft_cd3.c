144f6c7f84db64383406e878aa81b049965a0286
minsepar <parkj12b@github.com>
2024-03-09 13:39:03 +0900

    .

--- builtin/ft_cd3.c
+++ builtin/ft_cd3.c
@@ -6,7 +6,7 @@
 /*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 23:15:09 by minsepar          #+#    #+#             */
-/*   Updated: 2024/03/09 12:12:43 by minsepar         ###   ########.fr       */
+/*   Updated: 2024/03/09 13:38:52 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,6 @@ void	set_pwd_old_pwd(t_cmd_node *cmd_node,
 	ft_setenv(minishell->env, "OLDPWD", old_pwd);
 	ft_setenv(minishell->export, "PWD", minishell->cwd);
 	ft_setenv(minishell->env, "PWD", minishell->cwd);
-	ft_printf("%s\n", info->cur_path);
 	if (info->cur_path)
 		free(info->cur_path);
 	free(old_pwd);
