b97390b8c494e43097739de1b1eb15b2845d5e2b
minsepar <parkj12b@github.com>
2024-03-13 15:35:52 +0900

    .

--- subsystem/process_extern_command2.c
+++ subsystem/process_extern_command2.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   process_extern_command2.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sunghwki <sunghwki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: minsepar <minsepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 16:39:43 by minsepar          #+#    #+#             */
-/*   Updated: 2024/03/08 12:51:59 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/03/13 15:34:15 by minsepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../main.h"
 
 void	process_extern_cmd_no_slash(t_cmd_node *cmd_node,
-	t_minishell *minishell)
+	t_minishell *minishell, char **envp)
 {
 	char	*execute_path;
 
@@ -21,6 +21,6 @@ void	process_extern_cmd_no_slash(t_cmd_node *cmd_node,
 		command_not_found_error(minishell, cmd_node->cmd_name);
 	execute_path = find_from_path(cmd_node, minishell);
 	check_file_valid(execute_path, cmd_node, minishell);
-	if (execve(execute_path, cmd_node->str, minishell->envp) == -1)
+	if (execve(execute_path, cmd_node->str, envp) == -1)
 		shell_error(minishell, cmd_node->cmd_name, cmd_node->str[1]);
 }
